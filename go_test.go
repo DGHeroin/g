8c469cf3acaccbb7361d2b185267d6dc813ecdf7
Blake Li <6779586+DGHeroin@users.noreply.github.com>
2020-09-24 10:28:19 +0800

    Update go_test.go

--- go_test.go
+++ go_test.go
@@ -1,16 +1,16 @@
 package g
 
 import (
-    "fmt"
+    "errors"
     "sync"
     "testing"
     "time"
 )
 
-func EchoI(g *Go, i int, wg *sync.WaitGroup)  {
+func EchoI(t *testing.T,g *Go, i int, wg *sync.WaitGroup)  {
     wg.Add(1)
     g.Go(func() {
-        fmt.Println(i)
+        t.Log(i)
         time.Sleep(time.Second*time.Duration(i%4))
         wg.Done()
     })
@@ -19,8 +19,28 @@ func EchoI(g *Go, i int, wg *sync.WaitGroup)  {
 func TestNew(t *testing.T) {
     g := New(3)
     wg := sync.WaitGroup{}
-    for i := 0; i < 15; i++ {
-        EchoI(g, i, &wg)
+    for i := 0; i < 10; i++ {
+        EchoI(t, g, i, &wg)
     }
     wg.Wait()
+}
+
+func TestGo_Waterfall(t *testing.T) {
+    g := New(3)
+    g.Waterfall(func(err error) {
+        t.Log("结果", err)
+    }, func(err error, next func(error)) {
+        t.Log("1")
+        next(nil)
+    }, func(err error, next func(error)) {
+        t.Log("2")
+        next(nil)
+    }, func(err error, next func(error)) {
+        t.Log("3")
+        next(errors.New("error in 3"))
+        //next(nil)
+    }, func(err error, next func(error)) {
+        t.Log("4")
+        next(nil)
+    })
 }
